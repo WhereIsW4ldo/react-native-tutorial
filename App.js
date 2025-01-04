63a7aaf5330c06ce7e8a7628425db0ced7cf3b52
waldo wautelet <waldowautelet4@gmail.com>
2025-01-04 11:50:17 +0100

    Creating a default scrollView to play around with some RN components

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, ScrollView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>This is some text I just edited!</Text>
+        <Text>Another text block?</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+        <Text>Another text block in another view block?</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          defaultValue="DefaultValue of textInput"/>
+      </View>
+    </ScrollView>
   );
 }
 
